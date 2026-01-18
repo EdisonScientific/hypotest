d7a09462a0ee82003f4ce0ccd457d0ce41352ada
Siddharth Narayanan <sidnarayanan@users.noreply.github.com>
2026-01-18 14:54:40 -0800

    Notebook interface (#4)

--- src/hypotest/benchmark_agent.py
+++ src/hypotest/benchmark_agent.py
@@ -2,13 +2,15 @@ import argparse
 import asyncio
 import json
 import os
+import pickle
 from pathlib import Path
-from typing import Literal, Self
+from typing import Literal, Self, cast
 
 import yaml
 from aviary.core import TaskDatasetClient
-from ldp.agent import AgentConfig
+from ldp.agent import AgentConfig, SimpleAgent
 from ldp.alg import RolloutManager
+from ldp.data_structures import Trajectory
 from pydantic import BaseModel, ConfigDict, Field, FilePath, field_validator, model_validator
 from tqdm.asyncio import tqdm
 
@@ -48,22 +50,27 @@ async def main() -> None:
     config = BenchmarkConfig.model_validate(yaml.safe_load(config_path.read_text()))
 
     client = TaskDatasetClient(config.server_url, api_key=config.api_key, request_timeout=600)
-    agent = config.agent_config.construct_agent()
+    agent = cast(SimpleAgent, config.agent_config.construct_agent())
     semaphore = asyncio.Semaphore(config.num_parallel)
     rm = RolloutManager(agent)
 
-    async def rollout(idx: int) -> float:
+    async def rollout(idx: int) -> tuple[Trajectory, float]:
         async with semaphore:
             env = client.get_new_env_by_idx(idx)
             trajectory, *_ = await rm.sample_trajectories(environments=[env])
+            trajectory.traj_id = f"task_{idx}"
             # assume only terminal reward
-            return trajectory.steps[-1].reward
+            return trajectory, trajectory.steps[-1].reward
 
     results = await tqdm.gather(*[rollout(i) for i in range(len(client))], ncols=0, desc="Rollouts")
+    trajectories, rewards = zip(*results, strict=True)
 
-    (config.results_dir / "rewards.json").write_text(json.dumps(results, indent=2))
-    avg_reward = sum(results) / len(results)
-    frac_solved = sum(1 for r in results if r == 1.0) / len(results)
+    (config.results_dir / "rewards.json").write_text(
+        json.dumps({t.traj_id: r for t, r in zip(trajectories, rewards, strict=True)}, indent=2)
+    )
+    (config.results_dir / "trajectories.pkl").write_bytes(pickle.dumps(trajectories))
+    avg_reward = sum(rewards) / len(rewards)
+    frac_solved = sum(1 for r in rewards if r == 1.0) / len(rewards)
     print(f"Average reward: {avg_reward:.2f}")
     print(f"Fraction solved: {frac_solved:.2f}")
 
